5c104effbd482d14fcdc5204eab237a7c6463b31
user <user@JBTC10112>
2018-09-06 11:23:41 +0900

    18.09.06

--- 자바기초실습/src/제03차시/실습03.java
+++ 자바기초실습/src/제03차시/실습03.java
@@ -1,27 +1,69 @@
 package 제03차시;
 
+import javax.swing.plaf.synth.SynthSplitPaneUI;
+
 public class 실습03 {
 
 	public static void main(String[] args) {
-		int a=0, b=0;
-		a++; b++;
-		a+=15; b+=a;
-		a-=1; b*=a;
-		a/=2; b%=3;
-		a&=13; b|=a;
-		System.out.println("a="+a+",b="+b);
-		
-		double c=5.5 , d=3, e;
-		e=c%d;
+		int a = 0, b = 0;
+		a++;
+		b++;
+		a += 15;
+		b += a;
+		a -= 1;
+		b *= a;
+		a /= 2;
+		b %= 3;
+		a &= 13;
+		b |= a;
+		System.out.println("a=" + a + ",b=" + b);
+
+		double c = 5.5, d = 3, e;
+		e = c % d;
 		System.out.println(e);
-		String f1="통신과";
-		String f2="통신과";
-		String g1= new String("통신과");
-		String g2= new String("통신과");
-		System.out.println((f1==f2)+","+(g1==g2));
-		System.out.println(f1.equals(f2)+","+g1.equals(g2));
-		System.out.println(f1.hashCode()+","+f2.hashCode());
-		System.out.println(g1.hashCode()+","+g2.hashCode());
+
+		String f1 = "통신과";
+		String f2 = "통신과";
+		String g1 = new String("통신과");
+		String g2 = new String("통신과");
+		System.out.println((f1 == f2) + "," + (g1 == g2));
+		System.out.println(f1.equals(f2) + "," + g1.equals(g2));
+
+		System.out.println(System.identityHashCode(f1) + "," + System.identityHashCode(f2) + System.identityHashCode(g1)
+				+ "," + System.identityHashCode(g2));
+
+		// 비트 연산자 : 교과서 33쪽
+		System.out.println(0b00000110 & 0b11111101);
+		System.out.println(0b00000110 | 0b00001111);
+		System.out.println(0b00000110 ^ 0b00001111);
+		System.out.println(~0b00000110);
+		System.out.println(0b00010100 >> 2);
+		System.out.println(0b00010100 << 2);
+
+		System.out.println(0b10010100 >> 2);
+		System.out.println(0b10010100 >>> 2);
+
+		System.out.println(0b10000000000000000000000000010100 >> 2);
+		System.out.println(0b11100000000000000000000000000101);
+
+		System.out.println(0b10000000000000000000000000010100 >>> 2);
+		System.out.println(0b00100000000000000000000000000101);
+
+		System.out.println(-1 >> 2);
+		System.out.println(-1 >>> 2);
+
+		System.out.printf("%d, %d \n", 0b1 << 31, (0b1 << 31) - 1);
+
+		// 조건 연산자(삼항 연산자) : 교과서 34쪽
+		a = 2;
+		b = 3;
+		System.out.println("큰 값: " + (a > b ? a : b));
+
+		int o, p, q;
+		o = 2;
+		p = 10;
+		q = 15;
+		System.out.println("최대값: " + (o > p && o > q ? o : (p > o && o > q ? p : q)));
 	}
 
 }
---
+++ 자바기초실습/src/제03차시/실습04.java
@@ -0,0 +1,50 @@
+package 제03차시;
+
+public class 실습04 {
+
+	public static void main(String[] args) {
+		//if문의 예
+		int n=11;
+		if(n%2==0)
+			System.out.println(n+"은 짝수입니다.");
+		
+		else
+			System.out.println(n+"은 홀수입니다.");
+		
+		//switch문의 예
+		int m=7;
+		switch(m) {
+		case 1: case 3: case 5: case 7: case  8: case 10: case 12:
+			System.out.println(m+"월의 마지막 날은 31일 입니다.");
+			
+			break;
+		case 2:
+			System.out.println(m+"월의 마지막 날은  28일 입니다.");
+			
+			break;
+			default:
+				System.out.println(m+"월의 마지막 날은 30일  입니다.");
+		}
+
+		//for문의 예
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+		
+		//while문의 예
+		int i=1;
+		while(i<=10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		//dowhile문의 예
+		i=1;
+		do {
+			System.out.println(i);
+			i++;
+		} while(i<=10);
+		
+	}
+
+}
